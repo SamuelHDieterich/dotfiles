18759e55ff4c80e1ae70a4e5e6e7fda386201b5f
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2024-12-10 15:59:15 -0300

    ✨ Improve ZSH config
    - Copy from buffer
    - Accept next word

--- home-manager/modules/shell/zsh.nix
+++ home-manager/modules/shell/zsh.nix
@@ -4,17 +4,42 @@
   lib,
   ...
 }: {
+
+  home.packages = with pkgs; [
+    eza
+    wl-clipboard
+  ];
+
   programs.zsh = {
     enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    autocd = true;
 
+    initExtra = lib.concatStrings [
+      # Copy current buffer to clipboard
+      "
+      cmd_to_clip () { wl-copy -n <<< $BUFFER }
+      zle -N cmd_to_clip
+      bindkey '^Y' cmd_to_clip
+      "
+      # Autocomplete: accept next word
+      "bindkey '^[[Z' forward-word"
+    ];
     shellAliases = {
+      ls = "eza --icons=always --color";
       ll = "ls -l";
     };
+
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
