a99af85b719968ac81211e4666b330c486a5a92b
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2024-12-28 15:17:52 -0300

    ♻️ Refactor ZSH config setup

--- home-manager/modules/shell/zsh.nix
+++ home-manager/modules/shell/zsh.nix
@@ -20,19 +20,17 @@
     syntaxHighlighting.enable = true;
 
     # Get a Keystroke: CTRL+V + <key combination>
-    initExtra = lib.concatStrings [
+    initExtra = lib.strings.concatStringsSep "\n" [
       # Copy current buffer to clipboard
-      "
-      cmd_to_clip () { wl-copy -n <<< $BUFFER }
-      zle -N cmd_to_clip
-      bindkey '^Y' cmd_to_clip
-      "
+      "cmd_to_clip () { wl-copy -n <<< $BUFFER }"
+      "zle -N cmd_to_clip"
+      "bindkey '^Y' cmd_to_clip"
       # Autocomplete: accept next word
-      "
-      bindkey '^[[Z' forward-word
-      bindkey '^[[1;5C' forward-word
-      bindkey '^[[1;5D' backward-word
-      "
+      "bindkey '^[[Z' forward-word"
+      "bindkey '^[[1;5C' forward-word"
+      "bindkey '^[[1;5D' backward-word"
+      # For a clean $HOME 
+      "compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
     ];
     shellAliases = {
       ls = "eza --icons=always --color=always";
