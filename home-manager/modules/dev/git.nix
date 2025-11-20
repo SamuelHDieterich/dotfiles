55905b30edd6a5c15db0e7e326b4b8c6bff4940a
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2025-11-20 19:57:48 -0300

    👽 Update Git config

--- home-manager/modules/dev/git.nix
+++ home-manager/modules/dev/git.nix
@@ -2,8 +2,10 @@
   programs = {
     git = {
       enable = true;
-      userName = "Samuel Huff Dieterich";
-      userEmail = "72668845+SamuelHDieterich@users.noreply.github.com";
+      settings.user = {
+        name = "Samuel Huff Dieterich";
+        email = "72668845+SamuelHDieterich@users.noreply.github.com";
+      };
     };
     lazygit.enable = true;
     gh.enable = true;
