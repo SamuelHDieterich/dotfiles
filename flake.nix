61b7cf803018378edd93404324d96f4830bc5e24
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2025-11-23 12:14:49 -0300

    💥 Update flake definitions

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS configuration";
+  description = "Dotfiles configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,10 +7,12 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Disk management
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Secure boot
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,23 +22,27 @@
   outputs = inputs@{ nixpkgs, home-manager, disko, lanzaboote, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { };
+      };
     in {
+      # NixOS configurations
       nixosConfigurations = {
         home = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
             disko.nixosModules.disko # Disk management module
-            ./profiles/home/disk-configuration.nix # Disk configuration
+            ./nixos/profiles/home/disk-configuration.nix # Disk configuration
             lanzaboote.nixosModules.lanzaboote # Secure boot
-            ./profiles/home/configuration.nix # Regular NiXOS configuration
+            ./nixos/profiles/home/configuration.nix # Regular NiXOS configuration
             # Home Manager as a NixOS module
             home-manager.nixosModules.home-manager
             {
               home-manager = {
                 useGlobalPkgs = true;
                 useUserPackages = true;
-                users.samuel = import ../home-manager/profiles/home.nix;
+                users.samuel = import ./home-manager/profiles/home.nix;
                 extraSpecialArgs = {
                   inherit system;
                   inherit inputs;
@@ -46,5 +52,19 @@
           ];
         };
       };
+      # Home Manager configurations for non-NixOS systems
+      homeConfigurations =
+        # Dynamically generate Home Manager configurations for each profile in home-manager/profiles
+        builtins.listToAttrs (map (file: {
+          name = builtins.replaceStrings [ ".nix" ] [ "" ] file;
+          value = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home-manager/profiles/${file} ];
+            extraSpecialArgs = {
+              inherit system;
+              inherit inputs;
+            };
+          };
+        }) (builtins.attrNames (builtins.readDir ./home-manager/profiles)));
     };
 }
