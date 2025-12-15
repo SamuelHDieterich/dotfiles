1399a0bf070d20a22aac05974176bf44c9d61668
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2025-12-15 17:23:30 -0300

    🔐 Add Hyprlock basic config

--- nixos/modules/wm/hyprland.nix
+++ nixos/modules/wm/hyprland.nix
@@ -5,6 +5,7 @@
       xwayland.enable = true; # enable XWayland
     };
   };
+  security.pam.services.hyprlock = { };
   # Optional, hint electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
