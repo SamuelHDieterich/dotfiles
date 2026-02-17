9f80610cbc5b34e8a7df7fe698c733f78cc87076
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2026-02-17 11:31:19 -0300

    🎮 Adjust game module

--- modules/misc/games.nix
+++ modules/misc/games.nix
@@ -4,13 +4,14 @@
     {
       programs = {
         steam.enable = true; # Steam launcher
-        # lutris.enable = true; # Lutris game manager
         gamemode.enable = true; # Performance optimization tool
-        # mangohud.enable = true; # Monitoring overlay for games
       };
 
       environment.systemPackages = with pkgs; [
+        lutris # Game manager
+        mangohud # Monitoring overlay for games
         ruffle # Flash emulator for playing old Flash games
+        zenity # RUffle dependency for file dialogs
       ];
     };
 }
