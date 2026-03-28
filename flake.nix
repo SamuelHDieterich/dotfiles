ab1fea1a13762c53ab4b832e0b5e9ec397433af2
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2026-03-28 20:52:47 -0300

    🔒 Update flake config and lock file

--- flake.nix
+++ flake.nix
@@ -6,15 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Modular flake structure
     flake-parts.url = "github:hercules-ci/flake-parts";
-    # Flake utilities (shared)
-    flake-utils.url = "github:numtide/flake-utils";
-    # Flake compatibility (shared)
-    flake-compat.url = "github:NixOS/flake-compat";
-    # Gitignore patterns (shared)
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     # Recursively import nix modules
     import-tree.url = "github:vic/import-tree";
     # User-level configuration management
@@ -27,16 +18,12 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Hardware support
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     # Secure boot
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        pre-commit.inputs = {
-          flake-compat.follows = "flake-compat";
-          gitignore.follows = "gitignore";
-        };
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     # Secrets management
     sops-nix = {
@@ -46,28 +33,17 @@
     # OpenGL for non-NixOS systems
     nixgl = {
       url = "github:nix-community/nixGL";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     # Development environment management
     devenv = {
       url = "github:cachix/devenv";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-        flake-compat.follows = "flake-compat";
-        git-hooks.inputs.gitignore.follows = "gitignore";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     # Dynamic monitor management for Hyprland
     hyprdynamicmonitors = {
       url = "github:fiffeek/hyprdynamicmonitors";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     mangowc = {
       url = "github:ananyatimalsina/mangowc"; # https://github.com/mangowm/mango/pull/667
