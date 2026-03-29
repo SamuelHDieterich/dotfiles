343d79a3470c9e0f251bdb202846adde3efc9e66
Samuel Huff Dieterich <72668845+SamuelHDieterich@users.noreply.github.com>
2026-03-28 21:57:17 -0300

    🐛 Add missing hardware config for turing

---
+++ hosts/turing/disko.nix
@@ -0,0 +1,90 @@
+{ inputs, ... }:
+{
+  # Copied from tesla configuration
+  flake.nixosModules.turing = {
+    imports = [ inputs.disko.nixosModules.disko ];
+    disko.devices = {
+      disk = {
+        main = {
+          type = "disk";
+          device = "/dev/nvme0n1";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                label = "boot";
+                name = "ESP";
+                type = "EF00";
+                size = "512M";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "defaults" ];
+                };
+              };
+              luks = {
+                label = "cryptroot";
+                size = "100%";
+                content = {
+                  name = "cryptroot";
+                  type = "luks";
+                  settings = {
+                    allowDiscards = true;
+                    bypassWorkqueues = true;
+                  };
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [
+                      "-L"
+                      "nixos"
+                      "-f"
+                    ];
+                    subvolumes = {
+                      "/root" = {
+                        mountpoint = "/";
+                        mountOptions = [
+                          "subvol=root"
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/home" = {
+                        mountpoint = "/home";
+                        mountOptions = [
+                          "subvol=home"
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "subvol=nix"
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/log" = {
+                        mountpoint = "/var/log";
+                        mountOptions = [
+                          "subvol=log"
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/swap" = {
+                        mountpoint = "/swap";
+                        swap.swapfile.size = "16G";
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
